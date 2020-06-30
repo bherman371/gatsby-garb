c520fad93d720211e29ff9a481a1dcc083a127be
Benjamin Herman <bherman371@gmail.com>
2020-06-30 11:31:27 -0400

    Added page-3

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,45 +7,48 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
+const getSiteMetadata = graphql`
+{
+  site {
+    siteMetadata {
+      title
+      author
+      createdAt
     }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+  }
+}`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default function Layout ({ children }) {
+  return (
+    <StaticQuery
+      query={getSiteMetadata}
+      render={data => (
+        <>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0 1.0875rem 1.45rem`,
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              Built by{' '} 
+              {data.site.siteMetadata.author} © {data.site.siteMetadata.createdAt}
+            </footer>
+          </div>
+        </>
+      )}
+    />
+  )
+}
